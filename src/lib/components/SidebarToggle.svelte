c16c3e55a3ce1ee794d3fcad17c3401f5c89254f
Jared Cervantes <jared@jaredcervantes.com>
2025-06-08 19:53:45 -0400

    fix sidebar

--- src/lib/components/SidebarToggle.svelte
+++ src/lib/components/SidebarToggle.svelte
@@ -15,8 +15,9 @@
 	aria-label="Toggle sidebar"
 	on:click={handleToggle}
 >
-	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<path d="M3 12H21M3 6H21M3 18H21" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+		<rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+		<line x1="8" y1="6" x2="8" y2="18"></line>
 	</svg>
 </button>
 
